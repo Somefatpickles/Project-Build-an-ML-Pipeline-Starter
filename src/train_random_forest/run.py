bbe6d86c50fa5b63073babc96f66a3e7d2c0247a
Somefatpickles <dustinjwhitehouse@gmail.com>
2026-02-07 16:37:12 -0800

    Added necessary code to run.py in train_random_forest directory

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -23,6 +23,7 @@ import wandb
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.pipeline import Pipeline, make_pipeline
+from mlflow.models import infer_signature
 
 
 def delta_date_feature(dates):
@@ -73,7 +74,7 @@ def go(args):
 
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)
     ######################################
 
     # Compute r2 and MAE
@@ -94,9 +95,15 @@ def go(args):
 
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
+    export_path = os.path.join("random_forest_dir", args.output_artifact)
+    signature = infer_signature(X_val, y_pred)
     # HINT: use mlflow.sklearn.save_model
+    
     mlflow.sklearn.save_model(
-        # YOUR CODE HERE
+        sk_pipe,
+        path=export_path,
+        serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
+        signature=signature,
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -119,7 +126,7 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
-    # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
@@ -162,7 +169,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        # YOUR CODE HERE
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder()
     )
     ######################################
 
@@ -225,7 +233,8 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
 
     sk_pipe = Pipeline(
         steps =[
-        # YOUR CODE HERE
+        ("preprocessor", preprocessor),
+        ("random_forest", random_forest)
         ]
     )
 
